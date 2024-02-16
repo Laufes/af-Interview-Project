4326c475aabe484d30664dbd9195505c44035781
Michał Magdziarz <magmich2@gmail.com>
2024-02-16 15:20:02 +0100

    Fixed items were not sold correctly

--- AF Interview Project/Assets/Scripts/Items/InventoryController.cs
+++ AF Interview Project/Assets/Scripts/Items/InventoryController.cs
@@ -1,32 +1,36 @@
 ﻿namespace AFSInterview.Items
 {
-	using System.Collections.Generic;
-	using UnityEngine;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-	public class InventoryController : MonoBehaviour
-	{
-		[SerializeField] private List<Item> items;
-		[SerializeField] private int money;
+    public class InventoryController : MonoBehaviour
+    {
+        [SerializeField] private List<Item> items;
+        [SerializeField] private int money;
 
-		public int Money => money;
-		public int ItemsCount => items.Count;
+        public int Money => money;
+        public int ItemsCount => items.Count;
 
-		public void SellAllItemsUpToValue(int maxValue)
-		{
-			for (var i = 0; i < items.Count; i++)
-			{
-				var itemValue = items[i].Value;
-				if (itemValue > maxValue)
-					continue;
-				
-				money += itemValue;
-				items.RemoveAt(i);
-			}
-		}
+        public void SellAllItemsUpToValue(int maxValue)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                int itemValue = items[i].Value;
 
-		public void AddItem(Item item)
-		{
-			items.Add(item);
-		}
-	}
+                if (itemValue > maxValue)
+                {
+                    continue;
+                }
+
+                money += itemValue;
+                items.RemoveAt(i);
+                --i;
+            }
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+    }
 }
